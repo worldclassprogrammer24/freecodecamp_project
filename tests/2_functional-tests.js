debf70f4f0792202160ca2329735deceba79b56d
Michael DITCHOU <ditchoudjaba@gmail.com>
2026-08-25 16:06:57 +0100

    iiiopylm commit

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,132 +9,147 @@ chai.use(chaiHttp.default || chaiHttp);
 suite('Functional Tests', function() {
   this.timeout(15000);
 
-  let testThreadId;
-  let testReplyId;
+  let thread1Id;
+  let thread2Id;
+  let reply1Id;
 
-  // Attente explicite de la connexion MongoDB
+  // Attente de la connexion active à MongoDB avant le premier test
   before(async function() {
     if (mongoose.connection.readyState !== 1) {
       await new Promise(resolve => mongoose.connection.once('open', resolve));
     }
   });
 
-  suite('API ROUTING FOR /api/threads/:board', function() {
-    
-    test('Creating a new thread: POST request to /api/threads/{board}', function(done) {
-      chai.request(server)
-        .post('/api/threads/fcc_test_board')
-        .send({ text: 'FCC Test Thread', delete_password: 'pass' })
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          done();
-        });
-    });
-
-    test('Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}', function(done) {
-      chai.request(server)
-        .get('/api/threads/fcc_test_board')
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.isArray(res.body);
-          assert.isAtLeast(res.body.length, 1);
-          testThreadId = res.body[0]._id;
-          done();
-        });
-    });
-
-    test('Reporting a thread: PUT request to /api/threads/{board}', function(done) {
-      chai.request(server)
-        .put('/api/threads/fcc_test_board')
-        .send({ thread_id: testThreadId })
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.text, 'reported');
-          done();
-        });
-    });
-
-    test('Deleting a thread with the incorrect password: DELETE request to /api/threads/{board}', function(done) {
-      chai.request(server)
-        .delete('/api/threads/fcc_test_board')
-        .send({ thread_id: testThreadId, delete_password: 'wrong' })
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.text, 'incorrect password');
-          done();
-        });
-    });
-
+  // 1. Creating a new thread
+  test('Creating a new thread: POST request to /api/threads/{board}', function(done) {
+    chai.request(server)
+      .post('/api/threads/fcc_strict_board')
+      .send({ text: 'Thread 1', delete_password: 'pass' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        done();
+      });
   });
 
-  suite('API ROUTING FOR /api/replies/:board', function() {
+  // 2. Viewing the 10 most recent threads
+  test('Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}', function(done) {
+    chai.request(server)
+      .get('/api/threads/fcc_strict_board')
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.isAtLeast(res.body.length, 1);
+        thread1Id = res.body[0]._id;
+        done();
+      });
+  });
 
-    test('Creating a new reply: POST request to /api/replies/{board}', function(done) {
-      chai.request(server)
-        .post('/api/replies/fcc_test_board')
-        .send({ thread_id: testThreadId, text: 'FCC Test Reply', delete_password: 'pass' })
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          done();
-        });
-    });
+  // 3. Deleting a thread with the incorrect password
+  test('Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password', function(done) {
+    chai.request(server)
+      .delete('/api/threads/fcc_strict_board')
+      .send({ thread_id: thread1Id, delete_password: 'wrong' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'incorrect password');
+        done();
+      });
+  });
 
-    test('Viewing a single thread with all replies: GET request to /api/replies/{board}', function(done) {
-      chai.request(server)
-        .get('/api/replies/fcc_test_board')
-        .query({ thread_id: testThreadId })
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.property(res.body, 'replies');
-          testReplyId = res.body.replies[0]._id;
-          done();
-        });
-    });
+  // 4. Deleting a thread with the correct password
+  test('Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password', function(done) {
+    chai.request(server)
+      .delete('/api/threads/fcc_strict_board')
+      .send({ thread_id: thread1Id, delete_password: 'pass' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'success');
+        done();
+      });
+  });
 
-    test('Reporting a reply: PUT request to /api/replies/{board}', function(done) {
-      chai.request(server)
-        .put('/api/replies/fcc_test_board')
-        .send({ thread_id: testThreadId, reply_id: testReplyId })
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.text, 'reported');
-          done();
-        });
-    });
+  // 5. Reporting a thread
+  test('Reporting a thread: PUT request to /api/threads/{board}', function(done) {
+    // Création d'un nouveau thread car thread1Id a été supprimé au test précédent
+    chai.request(server)
+      .post('/api/threads/fcc_strict_board')
+      .send({ text: 'Thread 2', delete_password: 'pass' })
+      .end(function(err, res) {
+        chai.request(server)
+          .get('/api/threads/fcc_strict_board')
+          .end(function(err, res) {
+            thread2Id = res.body[0]._id;
+            chai.request(server)
+              .put('/api/threads/fcc_strict_board')
+              .send({ thread_id: thread2Id })
+              .end(function(err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'reported');
+                done();
+              });
+          });
+      });
+  });
 
-    test('Deleting a reply with the incorrect password: DELETE request to /api/replies/{board}', function(done) {
-      chai.request(server)
-        .delete('/api/replies/fcc_test_board')
-        .send({ thread_id: testThreadId, reply_id: testReplyId, delete_password: 'wrong' })
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.text, 'incorrect password');
-          done();
-        });
-    });
+  // 6. Creating a new reply
+  test('Creating a new reply: POST request to /api/replies/{board}', function(done) {
+    chai.request(server)
+      .post('/api/replies/fcc_strict_board')
+      .send({ thread_id: thread2Id, text: 'Reply 1', delete_password: 'pass' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
 
-    test('Deleting a reply with the correct password: DELETE request to /api/replies/{board}', function(done) {
-      chai.request(server)
-        .delete('/api/replies/fcc_test_board')
-        .send({ thread_id: testThreadId, reply_id: testReplyId, delete_password: 'pass' })
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.text, 'success');
-          done();
-        });
-    });
+  // 7. Viewing a single thread with all replies
+  test('Viewing a single thread with all replies: GET request to /api/replies/{board}', function(done) {
+    chai.request(server)
+      .get('/api/replies/fcc_strict_board')
+      .query({ thread_id: thread2Id })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'replies');
+        assert.isArray(res.body.replies);
+        reply1Id = res.body.replies[0]._id;
+        done();
+      });
+  });
 
-    test('Deleting a thread with the correct password: DELETE request to /api/threads/{board}', function(done) {
-      chai.request(server)
-        .delete('/api/threads/fcc_test_board')
-        .send({ thread_id: testThreadId, delete_password: 'pass' })
-        .end(function(err, res) {
-          assert.equal(res.status, 200);
-          assert.equal(res.text, 'success');
-          done();
-        });
-    });
+  // 8. Deleting a reply with the incorrect password
+  test('Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password', function(done) {
+    chai.request(server)
+      .delete('/api/replies/fcc_strict_board')
+      .send({ thread_id: thread2Id, reply_id: reply1Id, delete_password: 'wrong' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'incorrect password');
+        done();
+      });
+  });
 
+  // 9. Deleting a reply with the correct password
+  test('Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password', function(done) {
+    chai.request(server)
+      .delete('/api/replies/fcc_strict_board')
+      .send({ thread_id: thread2Id, reply_id: reply1Id, delete_password: 'pass' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'success');
+        done();
+      });
+  });
+
+  // 10. Reporting a reply
+  test('Reporting a reply: PUT request to /api/replies/{board}', function(done) {
+    chai.request(server)
+      .put('/api/replies/fcc_strict_board')
+      .send({ thread_id: thread2Id, reply_id: reply1Id })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'reported');
+        done();
+      });
   });
 
 });
